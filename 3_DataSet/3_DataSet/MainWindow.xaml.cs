d5e55d3aa7d9f3db577c3bd34af7f39b1a93e5b1
tw1xxxxx <133611442+tw1xxxxx@users.noreply.github.com>
2024-03-28 05:07:36 +0300

    t4g45
    5g45g45g

---
+++ 3_DataSet/3_DataSet/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _3_DataSet.storeP8DataSetTableAdapters;
+
+namespace _3_DataSet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        OrderArchiveTableAdapter orders = new OrderArchiveTableAdapter();
+        ProductsTableAdapter products = new ProductsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dateGrid.ItemsSource = orders.GetData();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            dateGrid.Columns[2].Visibility = Visibility.Collapsed;
+            dateGrid.Columns[2].Visibility = Visibility.Collapsed;
+            dateGrid.Columns[2].Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
---
+++ 3_Entity/3_Entity/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _3_Entity;
+
+namespace _3_Entity
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        private storeP8Entitieeees products = new storeP8Entitieeees();
+        private storeP8Entitieeees categories = new storeP8Entitieeees();
+        private storeP8Entitieeees orders = new storeP8Entitieeees();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var query = from o in products.OrderArchive
+                        join p in products.Products on o.Product_ID equals p.Product_ID
+                        join c in products.Categories on p.Category_ID equals c.Category_ID
+                        select new
+                        {
+                            o.OrderPrice,
+                            p.ProductName,
+                            p.Price,
+                            c.CategoryName
+                        };
+
+            dateGrid.ItemsSource = query.ToList();
+        }
+    }
+}
+
